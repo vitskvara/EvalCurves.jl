302abb3e22a1722c24db6e278140082a4230a13f
vit.skvara <vit.skvara@gmail.com>
2018-08-09 09:26:56 +0200

    updated tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,14 +8,14 @@ ascores = [0.1; 0.2; 0.3; 0.5; 0.4]
 N = size(labels,1)
 @testset "ALL" begin
 	fprvec, tprvec = EvalCurves.roccurve(ascores, labels)
-	@test abs(sum(tprvec - [0.0, 1.0, 1.0, 1.0, 1.0]))<1e-64
-	@test abs(sum(fprvec - [0.0, 0.25, 0.5, 0.75, 1.0]))<1e-64
+	@test abs(sum(tprvec - [0.0, 0.0, 1.0, 1.0, 1.0, 1.0]))<1e-64
+	@test abs(sum(fprvec - [0.0, 0.25, 0.25, 0.5, 0.75, 1.0]))<1e-64
 	auroc = EvalCurves.auc(fprvec, tprvec)
-	@test auroc==0.875
+	@test auroc==0.75
 	aauroc = EvalCurves.auc(fprvec, tprvec, "1/x")
-	@test round(aauroc, 4)==1.5833
+	@test round(aauroc, 4)==1.0833
 	caauroc = EvalCurves.auc(fprvec, tprvec, "centered")
-	@test round(caauroc, 4)==2.3524
+	@test round(caauroc, 4)==1.3524
 end
 
 
