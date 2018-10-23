64dbccaeaef17aa33cc661639d91c092f1dab582
honzabim <honzabim@gmail.com>
2018-10-23 07:36:15 +0200

    I changed the functions that we implemented with Pevnak.

--- src/utils.jl
+++ src/utils.jl
@@ -20,13 +20,13 @@ function roccurve(score::Vector, labels :: Vector)
     fprvec[1] = fpr # fp/n
     tprvec[1] = tpr # tp/p
     sortidx = sortperm(score)
-    sorted_labels = labels[sortidx]; 
-    sorted_scores = score[sortidx]; 
+    sorted_labels = labels[sortidx];
+    sorted_scores = score[sortidx];
     curveidx = 2
     for i in 2:N
-        if sorted_labels[i-1] == 0 
+        if sorted_labels[i-1] == 0
             fpr -=  1/n
-        else 
+        else
             tpr -= 1/p
         end
         if sorted_scores[i] != sorted_scores[i - 1]
@@ -38,9 +38,9 @@ function roccurve(score::Vector, labels :: Vector)
 
     # ensure zeros
     curveidx += (tprvec[curveidx] == 0 && fprvec[curveidx] == 0) ? 0 : 1
-    tprvec = tprvec[1:curveidx] 
-    fprvec = fprvec[1:curveidx] 
-    
+    tprvec = tprvec[1:curveidx]
+    fprvec = fprvec[1:curveidx]
+
     # sort out numerical -0
     fprvec = abs.(round.(fprvec; digits = 10))
     tprvec = abs.(round.(tprvec; digits = 10))
@@ -52,7 +52,7 @@ function roccurve(score::Vector, labels :: Vector)
     ist = sortperm(tprvec)
     tprvec = tprvec[ist]
     fprvec = fprvec[ist]
-    
+
     # this creates a semi-concave envelope of the roc curve
     # experimental
     #if concave
@@ -100,11 +100,11 @@ function auc(x,y, weights = "same")
         x = (x[2:end] + x[1:end-1])/2 # this is used for symmetric case
         inz = x.!=0 # nonzero indices
         w = 1 ./x[inz]
-        a = (y[1:end-1] + dy/2)[inz] 
+        a = (y[1:end-1] + dy/2)[inz]
         a = a.*w
         b = dx[inz]
     end
-    
+
     return dot(a,b)
 end
 
@@ -113,14 +113,14 @@ auc(x::Tuple, weights = "same") = auc(x..., weights)
 """
     auc_at_p(x,y,p,[weights,normalize])
 
-Compute the left 100*p% of the integral under (x,y). 
+Compute the left 100*p% of the integral under (x,y).
 """
 function auc_at_p(x,y,p,weights="same";normalize=false)
     @assert 0.0 <= p <= 1.0
     # get the x up to which we integrate
     px = maximum(x)*p + minimum(x)*(1-p)
     # now, this is done so that the resulting (_x,_y)
-    # under which is integrated ends correctly, 
+    # under which is integrated ends correctly,
     # otherwise integrals will not sum up as part will be ommited
     inds = x.<=px
     py = tpr_at_fpr(x,y,px)
@@ -144,7 +144,7 @@ function plotroc(args...)
 	title("ROC")
 
     # plot the diagonal line
-    plot(range(0,stop=1,length=100), range(0,stop=1,length=100), 
+    plot(range(0,stop=1,length=100), range(0,stop=1,length=100),
         c = "gray", alpha = 0.5, label = "")
     for arg in args
         plot(arg[1], arg[2], label = arg[3], lw = 2)
@@ -157,7 +157,7 @@ end
 ###########################################
 
 """
-   binarize(x) 
+   binarize(x)
 
 Transform x to binary labels if needed.
 """
@@ -176,13 +176,13 @@ function binarize(x::Vector)
     end
 end
 
-true_positive_inds(y_true::Vector, y_pred::Vector) = 
+true_positive_inds(y_true::Vector, y_pred::Vector) =
     (binarize(y_true).==1) .& (binarize(y_pred).==1)
-true_negative_inds(y_true::Vector, y_pred::Vector) = 
+true_negative_inds(y_true::Vector, y_pred::Vector) =
     (binarize(y_true).==0) .& (binarize(y_pred).==0)
-false_positive_inds(y_true::Vector, y_pred::Vector) = 
+false_positive_inds(y_true::Vector, y_pred::Vector) =
     (binarize(y_true).==0) .& (binarize(y_pred).==1)
-false_negative_inds(y_true::Vector, y_pred::Vector) = 
+false_negative_inds(y_true::Vector, y_pred::Vector) =
     (binarize(y_true).==1) .& (binarize(y_pred).==0)
 
 true_positive(y_true, y_pred) = sum(true_positive_inds(y_true, y_pred))
@@ -245,7 +245,7 @@ end
 
 Returns accuracy = (tp+tn)/(p+n).
 """
-accuracy(y_true, y_pred) = 
+accuracy(y_true, y_pred) =
     (true_positive(y_true, y_pred) + true_negative(y_true, y_pred))/length(y_true)
 
 """
@@ -315,15 +315,15 @@ mcc(y_true, y_pred) = matthews_correlation_coefficient(y_true, y_pred)
 """
    precision_at_k(y_true, y_pred, score, k)
 
-Precision at k most anomalous samples. 
+Precision at k most anomalous samples.
 """
 function precision_at_k(y_true, y_pred, score::Vector, k::Int)
-    lt, lp, las = length(y_true), length(y_pred), length(score)
-    @assert lt == lp == las
-    @assert all(k.<= (lt,lp,las))
+    lt, las = length(y_true), length(score)
+    @assert lt == las
+    @assert all(k .<= (lt, las))
     # sort anomaly scores from the largest
-    isort = sortperm(score,rev=true)
-    return precision(y_true[isort][1:k], y_pred[isort][1:k])
+	isort = sortperm(scores, rev = true)
+    return mean(y_true[isort][1:k])
 end
 
 """
@@ -344,22 +344,6 @@ function tpr_at_fpr(fpr::Vector, tpr::Vector, p::Real)
 end
 
 """
-    enclosed_volume(X, n_samples, predict_fun)
-
-Estimates the normalized volume enclosed by the decision boundary
-given a prediction function that return 1 for points inside and
-0 for points outside the boundary.
-"""
-function enclosed_volume(X::Matrix, n_samples::Int, predict_fun)
-    # limits of the hypercube
-    bounds = cat(minimum(X,dims=2),maximum(X,dims=2), dims=2)
-    # count hits inside and then simply divide them 
-    # by the total number of samples
-    hits = sum(predict_fun(rand(size(bounds,1),n_samples).*(bounds[:,2]-bounds[:,1]).+bounds[:,1]))
-    return 1. - hits/n_samples
-end
-
-"""
     threshold_at_fpr(y_true, score::Vector, p::Real)
 
 Returns a classifier threshold at given p% false positive rate.
@@ -368,37 +352,108 @@ function threshold_at_fpr(y_true, score::Vector, p::Real)
     N = length(score)
     @assert N == length(y_true)
     @assert 0.0 <= p .< 1.0
-    isort = sortperm(score,rev=true)
-    sorted_score = score[isort]    
-    sorted_labels = y_true[isort]
-    n = length(y_true) - sum(y_true)
-    # construct the fpr vector
-    fprs = zeros(N+1)
-    fpr = 0.0
-    for i in 1:N
-        if sorted_labels[i] == 0
-            fpr += 1/n
-        end
-        fprs[i] = fpr
+
+	descendingidx = sortperm(scores, rev = true)
+    scores = scores[descendingidx]
+    y_true = y_true[descendingidx]
+
+    distincvalueidx = findall(diff(scores) .!= 0)
+    thresholdidx = vcat(distincvalueidx, length(y_true))
+
+    tps = cumsum(y_true)[thresholdidx]
+    fps = thresholdidx .- tps
+    fps = fps ./ fps[end]
+
+    thresholds = scores[thresholdidx]
+
+    ids = fpr .>= fps
+    lastsmaller = sum(ids)
+    if lastsmaller == 0
+        @warn "No score to estimate lower FPR than $(fps[1])"
+        return NaN # thresholds[1]
+    elseif lastsmaller == length(fps)
+        @warn "No score to estimate higher FPR than $(fps[end])"
+        return NaN # thresholds[end]
     end
-    fprs[end] = 1.0
-    # now cut it at p
-    lefti = sum(fprs.<=p)
-    righti = lefti + 1
-    # now interpolate for tpr
+
+	lefti = lastsmaller
+	righti = lefti + 1
+	# now interpolate for tpr
     ratio = (p - fprs[lefti])/(fprs[righti] - fprs[lefti])
     return sorted_score[righti]*ratio + sorted_score[lefti]*(1-ratio)
 end
 
 """
+	sample_volume(score_fun, threshold, bounds, samples::Int = 10000)
+
+Samples one volume of the decision space where classifier marks samples as normal.
+"""
+function sample_volume(score_fun, threshold, bounds, samples::Int = 10000)
+    s = hcat(map(v -> length(v) == 2 ? rand(samples) .* (v[2] - v[1]) .+ v[1] : v[rand(1:length(v), samples)], bounds)...)
+    scores = score_fun(s)
+    return count(scores .<= threshold) / samples
+end
+
+function sample_volume(predict_fun, bounds, samples::Int = 10000)
+    s = hcat(map(v -> length(v) == 2 ? rand(samples) .* (v[2] - v[1]) .+ v[1] : v[rand(1:length(v), samples)], bounds)...)
+    hits = sum(predict_fun(s))
+    return 1. - hits / n_samples
+end
+
+"""
+	mc_volume_estimate(sample_volume_fun, iter::Int = 10)
+
+For added precision this samples the volume `iter` number of times and takes
+an average of that.
+"""
+function mc_volume_estimate(sample_volume_fun, iter::Int = 10)
+    volume = 0.
+    for i in 1:iter
+        volume += sample_volume_fun()
+    end
+    return volume / iter
+end
+
+"""
+	estimate_bounds(X::Matrix, threshold = 0.05)
+
+Estimates the bounds of a dataset. It sets maximum and minimum for continuous
+variables and saves all unique values for discrete ones. The decision boundary
+for continuous/discrete variable is #unique value/#samples <= `threshold`
+"""
+function estimate_bounds(X::Matrix, threshold = 0.05)
+	bounds = []
+	for i in 1:size(X, 1)
+		if (length(unique(X[i, :])) / length(X[i, :])) <= threshold
+			push!(bounds, unique(X[i, :]))
+		else
+			push!(bounds, vcat(minimum(X[i, :]), maximum(X[i, :])))
+		end
+	end
+	return bounds
+end
+
+"""
     volume_at_fpr(X, y_true, p, n_samples, predict_fun, ascore_fun, setthreshold_fun)
 
-Computes the 
+Computes the volume of space for which the classifier marks samples as normal.
+To achieve better precision, X should be a union of train and test set
 """
-function volume_at_fpr(X::Matrix, y_true, p::Real, n_samples::Int,
-    predict_fun, ascore_fun, setthreshold_fun)
+function volume_at_fpr(X::Matrix, y_true, p::Real, n_samples::Int, predict_fun, ascore_fun, setthreshold_fun)
     ascores = ascore_fun(X)
     threshold = threshold_at_fpr(y_true, ascores, p)
+	if threshold == NaN
+		return NaN
+	end
     setthreshold_fun(threshold)
-    return enclosed_volume(X, n_samples, predict_fun)
+    return mc_volume_estimate(() -> sample_volume(predict_fun, estimate_bounds(X))
+end
+
+function volume_at_fpr(X::Matrix, y_true, p::Real, n_samples::Int, ascore_fun)
+    ascores = ascore_fun(X)
+    threshold = threshold_at_fpr(y_true, ascores, p)
+	if threshold == NaN
+		return NaN
+	end
+    return mc_volume_estimate(() -> sample_volume(ascore_fun, threshold, estimate_bounds(X))
 end
